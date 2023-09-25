81d79bf446c29639433d491b15fe8515605cdbc7
Pablo Vacchelli <pablovacchelli@gmail.com>
2023-09-24 22:48:15 -0300

    cosas

---
+++ index.js
@@ -0,0 +1,17 @@
+let resultado = 0;
+
+let dolar = 735;
+
+let euro = 373;
+
+
+
+
+let divisa = prompt ('seleccionar dolar o euro');
+
+if (divisa = dolar) {
+    let valor = prompt('ingresar cantidad');
+    let resultado = valor/dolar;
+    console.log(resultado);
+
+}
